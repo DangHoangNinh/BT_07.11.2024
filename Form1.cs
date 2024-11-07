a611af12273d64aff5f0a9f00d3cdf43c4ae5332
DangHoangNinh <danghoangninh52@gmail.com>
2024-11-07 16:18:06 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace SimThe
+{
+    public partial class Form1 : Form
+    {
+        private DatabaseHelper dbHelper;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Khởi tạo DatabaseHelper mà không cần chuỗi kết nối
+            dbHelper = new DatabaseHelper();
+
+            // Tải dữ liệu từ cơ sở dữ liệu và hiển thị
+            LoadSimData();
+        }
+
+        private void LoadSimData()
+        {
+            string query = "SELECT Sim.ID, Sim.SoSim, Mang.TenMang, Sim.NgayKichHoat, Sim.NgayHetHan " +
+                           "FROM Sim " +
+                           "JOIN Mang ON Sim.MangID = Mang.ID " +
+                           "ORDER BY Sim.NgayKichHoat ASC";
+
+            DataTable simTable = dbHelper.GetData(query);
+            dataGridView1.DataSource = simTable;
+        }
+    }
+}
---
+++ SimThe.sql
@@ -0,0 +1,34 @@
+﻿-- Tạo Cơ Sở Dữ Liệu "SimThe"
+CREATE DATABASE SimThe;
+GO
+
+-- Chọn cơ sở dữ liệu "SimThe"
+USE SimThe;
+GO
+
+-- Tạo bảng "Mang" chứa thông tin nhà mạng
+CREATE TABLE Mang (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    TenMang NVARCHAR(50) NOT NULL
+);
+
+-- Tạo bảng "Sim" chứa thông tin về số sim
+CREATE TABLE Sim (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    SoSim NVARCHAR(15) NOT NULL,
+    MangID INT FOREIGN KEY REFERENCES Mang(ID),
+    NgayKichHoat DATE NOT NULL,
+    NgayHetHan DATE
+);
+GO
+
+-- Thêm một số nhà mạng mẫu vào bảng "Mang"
+INSERT INTO Mang (TenMang) VALUES ('Viettel');
+INSERT INTO Mang (TenMang) VALUES ('Mobifone');
+INSERT INTO Mang (TenMang) VALUES ('Vinaphone');
+
+-- Thêm một số số sim vào bảng "Sim"
+INSERT INTO Sim (SoSim, MangID, NgayKichHoat, NgayHetHan) VALUES ('0987654321', 1, '2022-01-01', '2023-01-01');
+INSERT INTO Sim (SoSim, MangID, NgayKichHoat, NgayHetHan) VALUES ('0901234567', 2, '2022-02-01', '2023-02-01');
+INSERT INTO Sim (SoSim, MangID, NgayKichHoat, NgayHetHan) VALUES ('0912345678', 3, '2022-03-01', '2023-03-01');
+GO
